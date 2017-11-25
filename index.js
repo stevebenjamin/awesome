992710bba8b9b51648d28a28dd5c2017d0326188
Steve Benjamin Avalos <fr3dth3@gmail.com>
2017-11-25 06:21:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send({ message: 'Its work'});
 })
 
+app.put('/update', (req, res) => {
+  console.log('updating....');
+  console.log(req.body);
+});
 app.listen(3000, () => {
   console.log('server running on http://localhost:3000');
 });
